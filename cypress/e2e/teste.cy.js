7e10ee80034cfa879cdde8a118057f866c3c2859
Guil_Fernandes <guilhermefernandes527@hotmail.com>
2025-11-10 20:17:01 -0300

    Esvaziar caixa de entrada 02

--- cypress/e2e/teste.cy.js
+++ cypress/e2e/teste.cy.js
@@ -38,7 +38,7 @@ it('valida o email na resposta da API', () =>{
     // ✅ roda uma única vez após TODOS os testes desta suíte
     cy.log('Depois de todos os testes');
     cy.get('div[class="wminboxheader"] > div > button[class="md but textu f24"]:not([id="delsel"])').click();
-    cy.get('[id="undodel"]').click();
+    cy.get('[id="delall"]').click();
     cy.on('window:confirm' , () => {
       return true
     })
