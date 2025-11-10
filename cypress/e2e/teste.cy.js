75b843497ee8ee12c4104e5af6047a5e39ed0c42
Guil_Fernandes <guilhermefernandes527@hotmail.com>
2025-11-10 20:22:58 -0300

    Esvaziar caixa de entrada 04

--- cypress/e2e/teste.cy.js
+++ cypress/e2e/teste.cy.js
@@ -42,7 +42,7 @@ it('valida o email na resposta da API', () =>{
     cy.on('window:confirm' , () => {
       return true
     })
-    cy.contains('Are you sure').click();
+    cy.click();
     cy.get('[id="message"]')
       .should('be.visible');
     });
